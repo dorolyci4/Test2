f8f34187a1665157396fb9c52450fce5ed6d6146
dorolyci4 <dorolyci4@gmail.com>
2020-11-01 01:31:23 -0500

    First Commit

---
+++ TestJunit/src/frsocom/Cours.java
@@ -0,0 +1,9 @@
+package frsocom;
+
+public class Cours {
+	public static int NbCours(int nb) {
+		System.out.println("Le nombre de cours est : "+nb);
+		return nb;
+	}
+
+}
---
+++ TestJunit/src/frsocom/Test.java
@@ -0,0 +1,46 @@
+package frsocom;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+
+
+public class Test {
+    
+	//@org.junit.Test
+	
+	@org.junit.Test(timeout = 1000)
+	public  void testNBCours() {
+			assertEquals(5, Cours.NbCours(5)); // TODO
+	}
+		
+
+	@BeforeClass
+	public static void testBeforeClass() {
+		System.out.println("Avant la classe");; // TODO
+	}
+	
+	@Before
+	public  void testBefore() {
+		System.out.println("Avant .....");; // TODO
+	}
+	
+	
+	@After
+	public  void testAfter() {
+		System.out.println("Après .....");; // TODO
+	}
+	
+	@AfterClass
+	public static void testAfterClass() {
+		System.out.println("Après Classe .....");; // TODO
+	}
+
+
+
+}
